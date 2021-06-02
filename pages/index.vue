445c8c4f8a51a75fe73cb9c779ef5a9dfd56ea2d
gokay <gokay.kahraman@yandex.com>
2021-06-02 15:14:57 +0300

    deneme

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ param }}
+  </div>
+</template>
+
+<script>
+const axios = require("axios")
+export default {
+  data() {
+    return {
+      param: null,
+    }
+  },
+    mounted: function() {
+    axios.get("/api/echo/hello-world")
+      .then(response => this.param = response.data)
+    },
+  
+} 
+</script>
+
+<style>
+
+</style>
